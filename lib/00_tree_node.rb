8ab54c3604a925230022b98c5976ad52531c2b81
Marcos <jmarcoshenrich@gmail.com>
2022-10-21 14:00:25 -0700

    ids done

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -10,19 +10,57 @@ class PolyTreeNode
     def parent=(parentnode)
         return if self.parent == parentnode
 
-            #C's children array - remove self from
         unless self.parent == nil
             self.parent.children.delete(self)
         end
-        @parent = parentnode
 
-        #nodeB is self
-        #nodeA is parentnode
+        @parent = parentnode
 
         unless @parent == nil    
             @parent.children << self
         end
     end
+
+    def add_child(child_node)
+        child_node.parent = self
+    end
+
+    def remove_child(child_node)
+        raise "This is not a child of this node" if child_node.parent != self
+        child_node.parent = nil
+        self.children.delete(child_node)
+    end
+
+    def dfs(target)
+        return nil if self == nil
+        return self if self.value == target
+
+        self.children.each do |child|
+            search_res = child.dfs(target)
+            return search_res unless search_res == nil
+        end
+        nil
+    end
+
+    def bfs(target)
+        array = [self]
+        
+        until array.empty?
+            if array.first.value == target
+                return array.first
+            else
+                array += array.shift.children
+            end
+        end
+        nil
+    end
+
+    #a b c
+    # b the child of a
+
+    # a.add_child(b)
+
+
 end
 
 # nodeG.parent = nodeB
