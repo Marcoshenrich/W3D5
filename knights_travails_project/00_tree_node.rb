a40624b1f9d74c6b1a2037d06327860e31435b6a
Marcos <jmarcoshenrich@gmail.com>
2022-10-21 14:59:06 -0700

    validmoves

---
+++ knights_travails_project/00_tree_node.rb
@@ -0,0 +1,96 @@
+class PolyTreeNode
+    attr_reader :parent, :children, :value
+
+    def initialize(value)
+        @parent = nil
+        @children = []
+        @value = value
+    end
+
+    def parent=(parentnode)
+        return if self.parent == parentnode
+
+        unless self.parent == nil
+            self.parent.children.delete(self)
+        end
+
+        @parent = parentnode
+
+        unless @parent == nil    
+            @parent.children << self
+        end
+    end
+
+    def add_child(child_node)
+        child_node.parent = self
+    end
+
+    def remove_child(child_node)
+        raise "This is not a child of this node" if child_node.parent != self
+        child_node.parent = nil
+        self.children.delete(child_node)
+    end
+
+    def dfs(target)
+        return nil if self == nil
+        return self if self.value == target
+
+        self.children.each do |child|
+            search_res = child.dfs(target)
+            return search_res unless search_res == nil
+        end
+        nil
+    end
+
+    def bfs(target)
+        array = [self]
+        
+        until array.empty?
+            if array.first.value == target
+                return array.first
+            else
+                array += array.shift.children
+            end
+        end
+        nil
+    end
+
+    #a b c
+    # b the child of a
+
+    # a.add_child(b)
+
+
+end
+
+# nodeG.parent = nodeB
+
+
+
+
+
+
+# node.parent = (parentnode)
+
+
+#        a
+#       / \
+#      b   c
+#     / \ / \
+#     d e f g 
+
+
+#        a
+#       / \
+#      b   c
+#    / / \  \ 
+#    g d e   f
+
+
+# OG
+# g.parent = c
+# c.children = [c,f]
+
+# switch to b
+# c.children = [g,f]
+# b.children = [g,d,e]
--- knights_travails_project/knights_travails.rb
+++ knights_travails_project/knights_travails.rb
@@ -1,26 +1,32 @@
-require_relative "tree_node_project/lib/00_tree_node.rb"
+require_relative "00_tree_node.rb"
 
 class KnightPathFinder 
+    attr_reader :cons_pos
+
     def initialize(start_pos)
         @start_pos = PolyTreeNode.new(start_pos)
         @cons_pos = [start_pos]
     end
 
-    def self.valid_move(pos)
+    def self.valid_moves(pos)
         row, col = pos
         valid_move_arr = []
-        valid_move_arr << [row - 2, col - 1] if KnightPathFinder.pos_checker([row - 2, col - 1])
-        knight_moves = [[-2, -1]
-        [-2, +1]
-        [-1, -2]
-        [-1, +2]
-        [+2, -1]
-        [+1, +2]
-        [+2, -1]
-        [+2, +1]]
+        knight_moves = [[-2, -1],[-2, 1],[-1, -2],[-1, 2],[1, -2],[1, 2],[2, -1],[2, 1]]
+
+        knight_moves.each do |knight_move|
+            row1 = row + knight_move[0]
+            col1 = col + knight_move[1]
+            pos1 = [row1, col1]
+            valid_move_arr << pos1 if KnightPathFinder.pos_checker(pos1)
+        end
+        valid_move_arr
     end
 
     def new_move_positions(pos)
+        validmoves = KnightPathFinder.valid_moves(pos)
+        validmoves.select! {|move| !@cons_pos.include?(move) }
+        @cons_pos.concat(validmoves)
+        validmoves
     end
 
     def self.pos_checker(pos)
@@ -33,14 +39,14 @@ class KnightPathFinder
 end
 
 #     0     1    2    3    4    5   6    7
-# 0 [[nil, nil, nil, nil, nil, nil, nil, nil],  
+# 0 [A, nil, nil, nil, nil, nil, nil, nil],  
 # 1 [nil, nil, nil, mov, nil, mov, nil, nil],
-# 2 [nil, nil, mov, nil, nil, nil, mov, nil],
+# 2 [nil, A2, mov, nil, nil, nil, mov, nil],
 # 3 [nil, nil, nil, nil, kgt, nil, nil, nil],
 # 4 [nil, nil, mov, nil, nil, nil, mov, nil],
 # 5 [nil, nil, nil, mov, nil, mov, nil, nil],
 # 6 [nil, nil, nil, nil, nil, nil, nil, nil],
-# 7 [nil, nil, nil, nil, nil, nil, nil, nil]]
+# 7 [nil, nil, nil, nil, nil, nil, nil, B]]
 
 # knight @ 3,4
 # moves 
@@ -48,7 +54,7 @@ end
 # [1, 5] [-2, +1]
 # [2, 2] [-1, -2]
 # [2, 6] [-1, +2]
-# [4, 2] [+2, -1]
+# [4, 2] [+1, -2]
 # [4, 6] [+1, +2]
 # [5, 3] [+2, -1]
 # [5, 5] [+2, +1]
