e0b39c9308b62a929748d8573ad92d604a375e77
Marcos <jmarcoshenrich@gmail.com>
2022-10-21 11:55:59 -0700

    parentnodebroken

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -1,3 +1,57 @@
 class PolyTreeNode
+    attr_reader :parent, :children, :value
 
+    def initialize(value)
+        @parent = nil
+        @children = []
+        @value = value
+    end
+
+    def parent=(parentnode)
+
+        self.parent.children - [self]    #C's children array - remove self from
+
+        @parent = parentnode
+
+
+
+        #nodeG is self
+        #nodeB is parentnode
+
+        unless parentnode == nil
+            parentnode.children << self
+        end
+    end
 end
+
+nodeG.parent = nodeB
+
+
+
+
+
+
+# node.parent = (parentnode)
+
+
+#        a
+#       / \
+#      b   c
+#     / \ / \
+#     d e f g 
+
+
+#        a
+#       / \
+#      b   c
+#    / / \  \ 
+#    g d e   f
+
+
+# OG
+# g.parent = c
+# c.children = [c,f]
+
+# switch to b
+# c.children = [g,f]
+# b.children = [g,d,e]
