232ed41b7f04e8187a40e3294a656ce67ff40392
Marcos <jmarcoshenrich@gmail.com>
2022-10-24 11:30:01 -0700

    children init

--- tic_tac/lib/tic_tac_toe_node.rb
+++ tic_tac/lib/tic_tac_toe_node.rb
@@ -2,6 +2,9 @@ require_relative 'tic_tac_toe'
 
 class TicTacToeNode
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
   end
 
   def losing_node?(evaluator)
@@ -12,6 +15,54 @@ class TicTacToeNode
 
   # This method generates an array of all moves that can be made after
   # the current move.
+
   def children
+
+
+    # iterate through the board
+    # get all valid positions (if empty)
+    # next move mark at every valid position
+    # iterate through valid 
+    # node.new(validmove)
+
+    valid_pos = []
+
+    @board.each_with_index do |row, i|
+      row.each_with_index do |el, z|
+        valid_pos << [i,z] if el == nil
+      end
+    end
+
+    
+
+    valid_pos.each do |pos|
+      row, col = pos
+      newboard = @board.dup
+      newboard[row][col] = @next_mover_mark
+      TicTacToeNode.new(newboard, ourmark, pos)
+    end
+
+
+
+    # curret node
+    # [nil,:X:,nil]
+    # [nil,nil,nil]
+    # [nil,nil,nil]
+
+
+    #-> 
+
+    #child 1
+    # [:0,:X:,nil]
+    # [nil,nil,nil]
+    # [nil,nil,nil]
+
+    #child 2
+    # [nil,:X:,:0]
+    # [nil,nil,nil]
+    # [nil,nil,nil]
+
+
   end
+  
 end
